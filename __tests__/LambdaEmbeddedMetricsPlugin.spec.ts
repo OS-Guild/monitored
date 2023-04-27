36dc2cfcaacb246b429ebc31440f58472a2022ca
MichaelV <mishaled@gmail.com>
2023-04-27 14:09:24 +0300

    Add lambda embedded metrics plugin (#1)

---
+++ __tests__/LambdaEmbeddedMetricsPlugin.spec.ts
@@ -0,0 +1,133 @@
+import {v4 as uuid} from 'uuid';
+import Monitor from '../src/Monitor';
+import {LambdaEmbeddedMetricsPlugin} from '../src/plugins/LambdaEmbeddedMetricsPlugin';
+import {pascalCase} from 'pascal-case';
+
+const pascalifyObject = (obj: Record<string, string>): Record<string, string> =>
+    Object.keys(obj).reduce((prev, curr) => {
+        prev[pascalCase(curr)] = obj[curr];
+        return prev;
+    }, {});
+
+const generateExpectedCall = (
+    serviceName: string,
+    metricName: string,
+    status: 'Start' | 'Success' | 'Failure',
+    unit: 'Count' | 'Seconds',
+    value: number,
+    context: {},
+    tags: {}
+) => ({
+    ...context,
+    ...tags,
+    _aws: {
+        CloudWatchMetrics: [
+            {
+                Dimensions: [Object.keys(tags)],
+                Metrics: [{Name: `${metricName}${status}`, Unit: unit}],
+                Namespace: serviceName,
+            },
+        ],
+    },
+    [`${metricName}${status}`]: value,
+});
+
+describe('LambdaEmbeddedMetricsPlugin', () => {
+    let plugin: LambdaEmbeddedMetricsPlugin;
+    let monitor: Monitor;
+    let serviceName: string;
+
+    function initMonitor(_opts?: LambdaEmbeddedMetricsPlugin) {
+        plugin = new LambdaEmbeddedMetricsPlugin({serviceName});
+        monitor = new Monitor({plugins: [plugin]});
+    }
+
+    beforeEach(() => {
+        jest.useFakeTimers();
+
+        serviceName = uuid();
+        initMonitor();
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+        jest.runOnlyPendingTimers();
+        jest.useRealTimers();
+    });
+
+    it('onSuccess', async () => {
+        const metricName = uuid();
+        const logSpy = jest.spyOn(console, 'log');
+        const tags = {[`dimension_${uuid()}`]: uuid(), [`dimension_${uuid()}`]: uuid()};
+        const expectedTags = pascalifyObject(tags);
+
+        const context = {[`property_${uuid()}`]: uuid(), [`property_${uuid()}`]: uuid()};
+        const expectedContext = pascalifyObject(context);
+
+        await monitor.monitored(metricName, async () => 123, {tags, context});
+
+        await monitor.flush(1000);
+        const {calls} = logSpy.mock;
+
+        const firstCall = JSON.parse(calls[0][0]);
+        delete firstCall._aws.Timestamp;
+        const lastCall = JSON.parse(calls[1][0]);
+        delete lastCall._aws.Timestamp;
+
+        expect(firstCall).toEqual(
+            expect.objectContaining(
+                generateExpectedCall(serviceName, metricName, 'Start', 'Count', 1, expectedContext, expectedTags)
+            )
+        );
+
+        expect(lastCall).toEqual(
+            expect.objectContaining(
+                generateExpectedCall(serviceName, metricName, 'Success', 'Seconds', 0, expectedContext, expectedTags)
+            )
+        );
+    });
+
+    it('onError', async () => {
+        const metricName = uuid();
+        const logSpy = jest.spyOn(console, 'log').mockImplementation(() => {});
+
+        const tags = {[`dimension_${uuid()}`]: uuid(), [`dimension_${uuid()}`]: uuid()};
+        const expectedTags = pascalifyObject(tags);
+
+        const context = {[`property_${uuid()}`]: uuid(), [`property_${uuid()}`]: uuid()};
+        const expectedContext = pascalifyObject(context);
+
+        try {
+            await monitor.monitored(
+                metricName,
+                async () => {
+                    throw new Error(uuid());
+                },
+                {tags, context}
+            );
+        } catch (err) {
+            console.log(err);
+        }
+
+        await monitor.flush(2000);
+
+        const {calls} = logSpy.mock;
+
+        const firstCall = JSON.parse(calls[0][0]);
+        delete firstCall._aws.Timestamp;
+        const lastCall = JSON.parse(calls[2][0]);
+        delete lastCall._aws.Timestamp;
+
+        expect(firstCall).toEqual(
+            expect.objectContaining(
+                generateExpectedCall(serviceName, metricName, 'Start', 'Count', 1, expectedContext, expectedTags)
+            )
+        );
+
+        expect(lastCall).toEqual(
+            expect.objectContaining(
+                generateExpectedCall(serviceName, metricName, 'Failure', 'Seconds', 0, expectedContext, expectedTags)
+            )
+        );
+    });
+});
---
+++ src/plugins/LambdaEmbeddedMetricsPlugin.ts
@@ -0,0 +1,91 @@
+import {pascalCase} from 'pascal-case';
+import {createMetricsLogger, Unit} from 'aws-embedded-metrics';
+import {MonitoredPlugin, OnFailureOptions, OnStartOptions, OnSuccessOptions} from './types';
+import {timeoutPromise} from '../utils';
+
+export interface CloudWatchPluginOptions {
+    serviceName: string;
+}
+
+const pascalifyObject = (obj: Record<string, string>): Record<string, string> =>
+    Object.keys(obj).reduce((prev, curr) => {
+        prev[pascalCase(curr)] = obj[curr];
+        return prev;
+    }, {});
+
+// https://docs.aws.amazon.com/lambda/latest/operatorguide/custom-metrics.html
+export class LambdaEmbeddedMetricsPlugin implements MonitoredPlugin {
+    private promises: Promise<void>[] = [];
+
+    constructor(readonly opts: CloudWatchPluginOptions) {}
+
+    private async sendMetric<T extends string>(
+        name: T,
+        unit: Unit,
+        value: number,
+        tags?: Record<string, string>,
+        context?: Record<string, string> | undefined
+    ): Promise<void> {
+        const metrics = createMetricsLogger();
+        metrics.setNamespace(this.opts.serviceName);
+
+        const dimensions = pascalifyObject(tags ?? {});
+        const properties = pascalifyObject(context ?? {});
+
+        metrics.setDimensions(dimensions);
+        Object.entries(properties).map(([x, y]) => metrics.setProperty(x, y));
+
+        metrics.putMetric(name, value, unit);
+
+        metrics.flushPreserveDimensions = false;
+        await metrics.flush();
+    }
+
+    async increment<T extends string>(
+        name: T,
+        value?: number | undefined,
+        tags?: Record<string, string> | undefined,
+        context?: Record<string, string> | undefined
+    ): Promise<void> {
+        await this.sendMetric(name, Unit.Count, value ?? 1, tags, context);
+    }
+
+    gauge(_name: string, _value?: number | undefined, _tags?: Record<string, string> | undefined): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+
+    async timing(
+        name: string,
+        value: number,
+        tags?: Record<string, string> | undefined,
+        context?: Record<string, string> | undefined
+    ): Promise<void> {
+        await this.sendMetric(name, Unit.Seconds, value, tags, context);
+    }
+
+    async flush(timeout: number): Promise<boolean> {
+        try {
+            await timeoutPromise(
+                timeout,
+                Promise.all(this.promises),
+                'Timeout reached, stopped wait for pending log writes'
+            );
+            return true;
+        } catch (err) {
+            console.log('sdfsd', err);
+            return false;
+        }
+    }
+
+    onStart({scope, options}: OnStartOptions): void {
+        this.promises.push(this.increment(`${scope}Start`, 1, options?.tags, options?.context));
+    }
+
+    onSuccess({scope, options, executionTime}: OnSuccessOptions): void {
+        this.promises.push(this.timing(`${scope}Success`, executionTime, options?.tags, options?.context));
+    }
+
+    onFailure({scope, options, executionTime}: OnFailureOptions): void {
+        this.promises.push(this.timing(`${scope}Failure`, executionTime, options?.tags, options?.context));
+    }
+}
